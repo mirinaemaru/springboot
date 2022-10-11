bb1f67247bdf92adbe854c5d56d9717b13c5c6a8
mirinae.maru <mirinae.maru@gmail.com>
2022-10-12 08:08:48 +0900

    Initial commit

---
+++ src/test/java/net/iotinfra/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iotinfra.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
